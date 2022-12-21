fe8c7f15b430acad6efcca0992e478b0c494d27f
Globally Used Name <globally@used.email>
2022-12-21 12:06:56 +0100

    test

--- drupal_node.py
+++ drupal_node.py
@@ -4,3 +4,4 @@ from drupal_configuration import *
 import re
 
 class DrupalNode():
+	"This is a super class that provides mutual attributes across all node types. Content type specific attributes have to be implemented in inheriting classes."
